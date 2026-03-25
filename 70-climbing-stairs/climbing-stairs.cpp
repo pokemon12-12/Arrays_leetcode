f1e92ab8bef60e719f9fd03bf6da9ae20a919844
Anish patel <patelanishh1212@gmail.com>
2026-03-25 22:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.71%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,8 +4,8 @@ public:
         if(n==1)return 1;
         if(n==2)return 2;
         if(n==3)return 3;
-        int a=1;
-        int b=2;
+        int a=1; //f(1)=1
+        int b=2; //f(2)=2
         int c;
         for(int i=3;i<=n;i++){
             c=a+b;
